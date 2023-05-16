22253a7f8cf3df2198cecf31fc38eb59b3843ac3
Rick Rietbergen <rbps3.2016@gmail.com>
2023-05-16 21:31:20 +0200

    add entities User, install pakages, add migrations, DbContext prorgam.cs

--- Entities/User.cs
+++ Entities/User.cs
@@ -1,6 +1,13 @@
-﻿namespace GeoProf.Entities
+﻿using GeoProf.Enums;
+
+namespace GeoProf.Entities
 {
     public class User
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+        public Role Role { get; set; }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using GeoProf.DataBase;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<GeoProfContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
