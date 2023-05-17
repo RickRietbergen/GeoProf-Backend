1b8f96dccf038ca1dcbb4968174d00d6948069a4
Rick Rietbergen <rbps3.2016@gmail.com>
2023-05-17 12:02:34 +0200

    Create BaseController, TryGetUserId, TryGetUserRoles.

---
+++ Controllers/BaseController.cs
@@ -0,0 +1,40 @@
+﻿using GeoProf.Enums;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GeoProf.Controllers
+{
+    public class BaseController : ControllerBase
+    {
+        protected bool TryGetUserId(out int? userId)
+        {
+            var result = HttpContext.Items.TryGetValue("user", out var user);
+
+            if (result)
+            {
+                userId = (int)user;
+                return true;
+            }
+            else
+            {
+                userId = null;
+                return false;
+            }
+        }
+
+        protected bool TryGetUserRoles(out Role? userRoles)
+        {
+            var result = HttpContext.Items.TryGetValue("roles", out var roles);
+
+            if (result)
+            {
+                userRoles = (Role)roles;
+                return true;
+            }
+            else
+            {
+                userRoles = null;
+                return false;
+            }
+        }
+    }
+}
