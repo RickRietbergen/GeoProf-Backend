4a42f1a5ee54b6205bc779a74e18770a7c154b18
Rick Rietbergen <rbps3.2016@gmail.com>
2023-05-24 11:51:34 +0200

    startup VerlofController

--- Controllers/VerlofController.cs
+++ Controllers/VerlofController.cs
@@ -1,7 +1,23 @@
-﻿namespace GeoProf.Controllers
-{
-    public class VerlofController
-    {
+﻿using GeoProf.DataBase;
+using GeoProf.Entities;
+using GeoProf.Enums;
+using GeoProf.Models;
+using GeoProf.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
+namespace GeoProf.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class VerlofController : BaseController
+    {
+        private readonly GeoProfContext dataContext;
+
+        public VerlofController(GeoProfContext dataContext)
+        {
+            this.dataContext = dataContext;
+        }
     }
 }
