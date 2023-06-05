bb91f780325a3b4d01f0a0f064e0b5fc7dca695c
Rick Rietbergen <rbps3.2016@gmail.com>
2023-06-05 19:58:17 +0200

    remove afdelingsId from verlof.cs

--- Entities/Verlof.cs
+++ Entities/Verlof.cs
@@ -4,7 +4,6 @@
     {
         public int Id { get; set; }
         public int UserId { get; set; }
-        public int AfdelingId { get; set; }
         public string VerlofReden { get; set; }
         public DateTime From { get; set; }
         public DateTime Until { get; set; }
