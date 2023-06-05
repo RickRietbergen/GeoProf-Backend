446fed81ab38cd777ab4a318c0550d1d42dd7cd1
Rick Rietbergen <rbps3.2016@gmail.com>
2023-06-05 21:36:49 +0200

    add seeder for afdelingen

---
+++ Seeders/DataSeeder.cs
@@ -0,0 +1,32 @@
+﻿using GeoProf.DataBase;
+using GeoProf.Entities;
+
+namespace GeoProf.Seeders
+{
+    public class DataSeeder
+    {
+        private readonly GeoProfContext dataContext;
+
+        public DataSeeder(GeoProfContext dataContext)
+        {
+            this.dataContext = dataContext;
+        }
+
+        public void Seed()
+        {
+            var afdelingen = new List<Afdeling>
+            {
+                new Afdeling { AfdelingNaam = "Office Management" },
+                new Afdeling { AfdelingNaam = "Geo ICT" },
+                new Afdeling { AfdelingNaam = "geodesy" },
+                new Afdeling { AfdelingNaam = "relation managment" },
+                new Afdeling { AfdelingNaam = "finance" },
+                new Afdeling { AfdelingNaam = "HRM" },
+                new Afdeling { AfdelingNaam = "ICT" }
+            };
+
+            dataContext.afdelingen.AddRange(afdelingen);
+            dataContext.SaveChanges();
+        }
+    }
+}
