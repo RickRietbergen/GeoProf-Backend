b75676b1f2b7327449d09214c0a32d951def1006
Rick Rietbergen <rbps3.2016@gmail.com>
2023-06-05 20:17:21 +0200

    add migrations

--- DataBase/GeoProfContext.cs
+++ DataBase/GeoProfContext.cs
@@ -1,4 +1,5 @@
 ﻿using GeoProf.Entities;
+using GeoProf.Migrations;
 using Microsoft.EntityFrameworkCore;
 
 namespace GeoProf.DataBase
@@ -12,5 +13,6 @@ namespace GeoProf.DataBase
 
         public DbSet<User> Users { get; set; }
         public DbSet<Verlof> Verlofs { get; set; }
+        public DbSet<Afdeling> afdelingen { get; set; }
     }
 }
---
+++ Migrations/20230605181451_Afdelingen.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GeoProf.Migrations
+{
+    /// <inheritdoc />
+    public partial class Afdelingen : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "afdelingen",
+                columns: table => new
+                {
+                    AfdelingId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AfdelingNaam = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_afdelingen", x => x.AfdelingId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "afdelingen");
+        }
+    }
+}
