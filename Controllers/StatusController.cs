8fe03e9d0ed733e4cd28e5923f80ca9ebe4dfd5e
Rick Rietbergen <rbps3.2016@gmail.com>
2023-06-13 23:05:56 +0200

    create function for set verlof aanvraag to denied

---
+++ Controllers/StatusController.cs
@@ -0,0 +1,38 @@
+﻿using GeoProf.DataBase;
+using GeoProf.Enums;
+using GeoProf.Attributes;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace GeoProf.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class StatusController : BaseController
+    {
+        private readonly GeoProfContext dataContext;
+
+        public StatusController(GeoProfContext dataContext)
+        {
+            this.dataContext = dataContext;
+        }
+
+        [HttpPut("denied/{id}")]
+        [JWTAuth(Role.manager | Role.admin)]
+        public async Task<IActionResult> Denied(int id)
+        {
+            var selectedAanvraag = await dataContext.Verlofs.Where(a => a.Id == id).FirstOrDefaultAsync();
+            if (selectedAanvraag == null)
+            {
+                return BadRequest("Verlof Request does not exist");
+            }
+            selectedAanvraag.IsPending = false;
+            selectedAanvraag.IsDenied = true;
+
+            await dataContext.SaveChangesAsync();
+
+            return Ok();
+        }
+    }
+}
